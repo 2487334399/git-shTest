2b3651cea581e3fb80a1c7bb3f50740ea2e39309
liuxing <2487334399@qq.com>
2022-02-16 23:45:15 +0800

    the first commit Calculate

---
+++ Calculate/src/class01/code02_BubbleSort.java
@@ -0,0 +1,17 @@
+package class01;
+
+public class code02_BubbleSort {
+
+    public static void bubbleSort(int[] arr){
+        if(arr == null || arr.length<2){
+            return;
+        }
+        for(int e=arr.length-1;e>0;e--){
+            for(int i=0;i<e;i++){
+                if(arr[i]>arr[i+1]){
+
+                }
+            }
+        }
+    }
+}
