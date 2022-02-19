aed14f78fa3a0a0ccacdf1ba9cf0022f458d0f6e
liuxing <2487334399@qq.com>
2022-02-20 00:18:42 +0800

    the fouth commit Calculate

--- Calculate/src/class01/code02_BubbleSort.java
+++ Calculate/src/class01/code02_BubbleSort.java
@@ -4,14 +4,17 @@ public class code02_BubbleSort {
 
     public static void bubbleSort(int[] arr){
         if(arr == null || arr.length<2){
+
             return;
         }
         for(int e=arr.length-1;e>0;e--){
             for(int i=0;i<e;i++){
                 if(arr[i]>arr[i+1]){
-
+                    System.out.println("==========");
+                    System.out.println("===========");
                 }
             }
         }
+
     }
 }
