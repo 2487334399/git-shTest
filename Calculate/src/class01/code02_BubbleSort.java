8ddb56b713df85f7a3a7917e96aff0d1eb4a418b
liuxing <2487334399@qq.com>
2022-02-20 00:54:20 +0800

    the third commit Calculate

--- Calculate/src/class01/code02_BubbleSort.java
+++ Calculate/src/class01/code02_BubbleSort.java
@@ -10,7 +10,7 @@ public class code02_BubbleSort {
         for(int e=arr.length-1;e>0;e--){
             for(int i=0;i<e;i++){
                 if(arr[i]>arr[i+1]){
-                    System.out.println("==========");
+                    System.out.println("毛熊:回到祖国还好吗");
                 }
             }
         }
