05eaad01b08c12a9021e040f216d4c1b6c1d7411
liuxing <2487344399@qq.com>
2022-03-08 01:06:23 +0800

    提交已经加入分支的代码

--- shejimoshi/src/at/guigu/com/SenderFactory.java
+++ shejimoshi/src/at/guigu/com/SenderFactory.java
@@ -3,13 +3,18 @@ package at.guigu.com;
 public class SenderFactory {
 
     public Sender produce(String type){
-        if("email".equals(type)){
-            return new MailSender();
-        }else if("Sms".equals(type)){
-            return new SmsSender();
+        if(type!=null){
+            if("email".equals(type)){
+                return new MailSender();
+            }else if("Sms".equals(type)){
+                return new SmsSender();
+            }else{
+                System.out.println("输入正确的类型");
+                return null;
+            }
         }else{
-            System.out.println("输入正确的类型");
             return null;
         }
+
     }
 }
