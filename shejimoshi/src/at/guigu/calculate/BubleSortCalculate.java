860c1538d401aa6912d65d0ec8908b2d100e3141
liuxing <2487344399@qq.com>
2022-04-01 21:53:48 +0800

    “第一次提交没有优化的冒泡排序” BubleSortCalculate

--- shejimoshi/src/at/guigu/calculate/BubleSortCalculate.java
+++ shejimoshi/src/at/guigu/calculate/BubleSortCalculate.java
@@ -5,9 +5,8 @@ public class BubleSortCalculate {
     public static void main(String[] args) {
         //好的计算
         int[] array={99,66,22,44,1,2,5,0,44};
-        //第二轮进行排序
-        for(int j= array.length-1;j>0;j--){
-            for (int i = 1; i <=j; i++) {
+        for(int j=array.length-1;j>0;j--){
+            for (int i = 1; i <= j; i++) {
                 if(array[i]<array[i-1]){
                     int temp=array[i];
                     array[i]=array[i-1];
@@ -15,9 +14,9 @@ public class BubleSortCalculate {
                 }
             }
         }
-        //从第一轮进行排序
-        for(int i=0;i<array.length;i++){
-            System.out.print(array[i]+"----");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("-"+array[i]);
         }
     }
 }
