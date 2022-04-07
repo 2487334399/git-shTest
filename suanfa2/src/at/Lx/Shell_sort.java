02c4f8254325b0edc26f84f3a1855b5112b7549f
liuxing <2487344399@qq.com>
2022-04-08 00:30:13 +0800

    提交希尔排序 Shell_Sort.java

---
+++ suanfa2/src/at/Lx/Shell_sort.java
@@ -0,0 +1,34 @@
+package at.Lx;
+
+public class Shell_sort {
+
+    public static void main(String[] args) {
+        int[]arr={12,43,22,11,55,77,32,57};
+        sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+",");
+        }
+    }
+    public static void sort(int[] arr) {
+        int length = arr.length;
+        //区间
+        int gap = 1;
+        while (gap < length) {
+            gap = gap * 3 + 1;
+        }
+        while (gap > 0) {
+            for (int i = gap; i < length; i++) {
+                int tmp = arr[i];
+                int j = i - gap;
+                //跨区间排序
+                while (j >= 0 && arr[j] > tmp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = tmp;
+            }
+            gap = gap / 3;
+        }
+    }
+
+}
