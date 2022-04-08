83e1715b1ad57eacb850571d84a17cc63e159e81
liuxing <2487344399@qq.com>
2022-04-09 01:15:32 +0800

    提交单例模式 version1

---
+++ Tewenty_Three_Shejimoshi/src/at/Dl3/SingletonTest.java
@@ -0,0 +1,20 @@
+package at.Dl3;
+
+public class SingletonTest {
+
+    private static SingletonTest instance=null;
+
+    private SingletonTest(){
+    }
+    private static synchronized void syncInit(){
+        if(instance==null){
+            instance=new SingletonTest();
+        }
+    }
+    public static SingletonTest getInstance(){
+        if(instance==null){
+            syncInit();
+        }
+        return instance;
+    }
+}
