f31f574198c2465120a8e6ae69e135d8a95a348a
liuxing <2487344399@qq.com>
2022-04-10 02:35:08 +0800

    提交两个数之和等于目标数的优化之后的数值

--- Cal/src/com/company/two/Num.java
+++ Cal/src/com/company/two/Num.java
@@ -1,6 +1,13 @@
 package com.company.two;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Num {
+    public static void main(String[] args) {
+        int[]arr={1,5,2,55,7};
+        twoSum(arr,62);
+    }
     public static int[] twoSum(int[]nums,int target){
         int[]result=new int[2];
         for(int i=0;i<nums.length;i++){
@@ -14,9 +21,22 @@ public class Num {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        int[]arr={1,5,2,55,7};
-        twoSum(arr,62);
+    //两个数之和得到优化
+    public int[] twoSum1(int[]nums,int target){
+        /*key为元素值，value为每个元素对应的下标*/
+        Map<Integer,Integer> storeNums=new HashMap<>(nums.length,1);
+        int[]result=new int[2];
+        for(int i=0;i< nums.length;i++){
+            int another=target-nums[i];
+            Integer anotherIndex=storeNums.get(another);
+            if(null!=anotherIndex){
+                result[0]=anotherIndex;
+                result[i]=1;
+                break;
+            }else{
+                storeNums.put(nums[i],i);
+            }
+        }
+        return result;
     }
 }
