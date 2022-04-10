a157d3e3ea6646c4265993f110431bb606b86fa2
liuxing <2487344399@qq.com>
2022-04-10 15:57:41 +0800

    提交两个合并数组的优化

--- Cal/src/com/company/Inte/Solution.java
+++ Cal/src/com/company/Inte/Solution.java
@@ -3,7 +3,7 @@ package com.company.Inte;
 import java.util.Arrays;
 
 public class Solution {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         Solution solution=new Solution();
     }
     public void merge(int[]nums1,int m,int[]nums2,int n){
@@ -11,5 +11,24 @@ public class Solution {
             nums1[m+i]=nums2[i];
         }
         Arrays.sort(nums1);
-    }
+    }*/
+     public static void merge(int[] nums1,int m,int[] nums2,int n){
+         int k=m+n;
+         int[]temp=new int[k];
+         for(int index=0,nums1Index=0,nums2Index=0;index<k;index++){
+             if(nums1Index>=m){/*nums1数组以及取完，完全取nums2数组的值即可*/
+                temp[index]=nums2[nums2Index++];
+             }else if(nums2Index>=n){/*nums2数组已经取完，完全取nums1数组的值即可*/
+                 temp[index]=nums1[nums1Index++];
+             }else if(nums1[nums1Index]<nums2[nums2Index]){
+                 /*nums1数组的元素值小于nums2数组,取nums1数组的值*/
+                 temp[index]=nums1[nums2Index++];
+             }else{/*nums2数组的元素值小于等于nums1左边数组,取nums2数组的值*/
+                 temp[index]=nums2[nums2Index++];
+             }
+         }
+         for(int i=0;i<k;k++){
+             nums1[i]=temp[i];
+         }
+     }
 }
