cf971866ea0a551b65a114f45dbefb31955c6d25
liuxing <2487344399@qq.com>
2022-04-10 22:59:05 +0800

    寻找缺少的数字

--- Cal/src/com/company/LR/Solution.java
+++ Cal/src/com/company/LR/Solution.java
@@ -5,24 +5,21 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        int[]arr={1,3,5,2,3,3,7,5};
-        findDisappearedNumbers(arr);
-
     }
     public static List<Integer> findDisappearedNumbers(int[] nums){
-        int n= nums.length;
-        for(int num:nums){
-            int x=(num-1)%n;/*对n取模还原出它本来的值*/
-            nums[x]+=n;
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;/*对n取模还原出它本来的值*/
+            nums[x] += n;//加n,让已有数值的元素的下表的值大于数组元素个数
         }
         List<Integer>result=new ArrayList<Integer>();
         for(int i=0;i<n;i++){
-            if(nums[i]<=n){
+            if(nums[i]<=n){//所以小于n的只有这里的两个元素
                 result.add(i+1);
             }
         }
         for(int a:result){
-            System.out.println(a);
+            System.out.print(a+" ");
         }
         return result;
     }
