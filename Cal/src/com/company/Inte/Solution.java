72abd5f198ed0c116e7a95d437617fe31a658c3e
liuxing <2487344399@qq.com>
2022-04-10 15:31:02 +0800

    提交两个合并数组

---
+++ Cal/src/com/company/Inte/Solution.java
@@ -0,0 +1,15 @@
+package com.company.Inte;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+    }
+    public void merge(int[]nums1,int m,int[]nums2,int n){
+        for(int i=0;i<n;++i){
+            nums1[m+i]=nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
