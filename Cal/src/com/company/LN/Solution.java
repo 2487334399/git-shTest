df559295098da4c360fb17d3fec55684172a15be
liuxing <2487344399@qq.com>
2022-04-12 23:18:26 +0800

    列表中是否有环形列表

---
+++ Cal/src/com/company/LN/Solution.java
@@ -0,0 +1,16 @@
+package com.company.LN;
+
+public class Solution {
+    public boolean hasCycle(ListNode head){
+        if(head==null) return false;
+        ListNode slowPtr=head,fastPtr=head;
+        while(fastPtr.next!=null&& fastPtr.next.next!=null){
+            slowPtr=slowPtr.next;
+            fastPtr=fastPtr.next.next;
+            if(slowPtr==fastPtr){
+                return true;
+            }
+        }
+        return false;
+    }
+}
