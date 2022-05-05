4d301872019118ac0d0481ebd1183107ddf43996
liuxing <2487344399@qq.com>
2022-05-06 00:18:29 +0800

    闭环判断提交version01

---
+++ Pro_Solution/src/tulingxueyuan/Cycle.java
@@ -0,0 +1,17 @@
+package tulingxueyuan;
+
+public class Cycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slowPtr = head, fastPtr = head;
+        while (fastPtr.next != null && fastPtr.next.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if (slowPtr == fastPtr) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
