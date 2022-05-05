b55a356bc9a1d9e2e23fb2ee0098444c73a09f43
liuxing <2487344399@qq.com>
2022-05-05 19:21:35 +0800

    数组去0的算法并把0归并到末尾的做法version1

---
+++ Pro_Solution/src/tulingxueyuan/MoveZero.java
@@ -0,0 +1,23 @@
+package tulingxueyuan;
+
+public class MoveZero {
+
+    public void moveZoroes(int[] nums){
+        if(nums==null){
+            return;
+        }
+        //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋值给
+        //nums[j]
+        int j=0;
+        for(int i=0;i<nums.length;++i){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+        //非0元素统计完了，剩下的都是0了
+        //所以第二次遍历把末尾的元素都赋为0即可
+        for(int i=j;j<nums.length;++i){
+            nums[i]=0;
+        }
+    }
+}
