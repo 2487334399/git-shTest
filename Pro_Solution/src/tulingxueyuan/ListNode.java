62fda59b38b302350423df383b1aee9321c8766f
liuxing <2487344399@qq.com>
2022-05-06 02:32:57 +0800

    闭环判断提交version01

--- Pro_Solution/src/tulingxueyuan/ListNode.java
+++ Pro_Solution/src/tulingxueyuan/ListNode.java
@@ -5,9 +5,14 @@ public class ListNode {
     int val;
 
     ListNode next;
+    ListNode(){
 
-    ListNode(int x){
-        val=x;
-        next=null;
+    }
+    ListNode(int val){
+        this.val=val;
+    }
+    ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
     }
 }
