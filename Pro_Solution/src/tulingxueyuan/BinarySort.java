d0cb20575e4c2d0cda08f6348ed412b091c88429
liuxing <2487344399@qq.com>
2022-05-06 23:36:24 +0800

    二分查找排序Binary version1

---
+++ Pro_Solution/src/tulingxueyuan/BinarySort.java
@@ -0,0 +1,23 @@
+package tulingxueyuan;
+
+public class BinarySort {
+    public void sort(int []array){
+        for(int i=1;i<array.length;i++){
+            int temp=array[i];
+            int low=0,high=i-1;
+            int mid=-1;
+            while(low<=high){
+                mid=low+(high-low)/2;
+                if(array[mid]>temp){
+                    high=mid-1;
+                }else{//元素相同时，也插入在后面的位置
+                    low=mid+1;
+                }
+            }
+            for(int j=i-1;j>low;j--){
+                array[j+1]=array[j];
+            }
+            array[low]=temp;
+        }
+    }
+}
