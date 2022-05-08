fa03121bf86d58fb897b056d83659494d76ee921
liuxing <2487334399@qq.com>
2022-05-08 22:35:28 +0800

    冒泡排序测试

--- Pro_Solution/src/com/company/Main.java
+++ Pro_Solution/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[]arr={11,2,4,5,1,5,35,66};
+        int[]arr={11,2,4,5,1,5,35,66,22,999};
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]>arr[j+1]){
@@ -12,8 +12,6 @@ public class Main {
                     arr[j+1]=temp;
                 }
             }
-            System.out.print(arr[i]+"--");
         }
-
     }
 }
