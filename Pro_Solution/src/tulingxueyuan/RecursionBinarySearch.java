302cc35dfb81ef5ec2dc159070f8e82229d6de7a
liuxing <2487334399@qq.com>
2022-05-08 18:28:15 +0800

    递归二分查找 和非递归二分查找 RecursionBinarySearch

--- Pro_Solution/src/tulingxueyuan/RecursionBinarySearch.java
+++ Pro_Solution/src/tulingxueyuan/RecursionBinarySearch.java
@@ -1,7 +1,7 @@
 package tulingxueyuan;
 
 public class RecursionBinarySearch {
-
+    //采用递归二分查找
     public static int recursionBinarySearch(int[]arr,int key,int low,int high){
         if(key<arr[low]||key>arr[high]||low>high){
             return -1;//没有要找的数字
@@ -16,5 +16,27 @@ public class RecursionBinarySearch {
         }else{
             return middle;
         }
+    }
+    //采用非递归查找
+    public static int commonBinarySearch(int []arr,int key){
+        int low=0;
+        int high=arr.length-1;
+        int middle=0;
+        if(key<arr[low]||key>arr[high]||low>high){
+            return -1;
+        }
+        while(low<=high){
+            middle=(low+high)/2;
+            if(arr[middle]>key){
+                //比关键字大则在关键子左区域
+                high=middle-1;
+            }else if(arr[middle]<key){
+                //比关键字小则在关键字右区域
+                low=middle+1;
+            }else{
+                return middle;
+            }
+        }
+        return -1;
     }
 }
