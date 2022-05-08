9eec9a03f629dc8d0a134ef1967fce10244e73ac
liuxing <2487344399@qq.com>
2022-05-08 17:32:18 +0800

    后续遍历 InOrder

---
+++ Pro_Solution/src/tulingxueyuan/PostOrder.java
@@ -0,0 +1,21 @@
+package tulingxueyuan;
+
+import java.util.ArrayList;
+import java.util.List;
+//后续遍历
+public class PostOrder {
+    public List<Integer> postOrderTest(TreeNode root){
+        List<Integer>res=new ArrayList<>();
+        postOrder(root,res);
+        return res;
+    }
+
+    public void postOrder(TreeNode root, List<Integer> res){
+        if(root==null){
+            return;
+        }
+        postOrder(root.left,res);
+        postOrder(root.right,res);
+        res.add(root.val);
+    }
+}
