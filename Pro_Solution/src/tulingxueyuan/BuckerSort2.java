412e76c1dfa96c12fb6ae3a562ef7ede95f07e6c
liuxing <2487334399@qq.com>
2022-05-08 21:20:04 +0800

    提交桶排序 BuckerSort2.java

--- Pro_Solution/src/tulingxueyuan/BuckerSort2.java
+++ Pro_Solution/src/tulingxueyuan/BuckerSort2.java
@@ -2,7 +2,16 @@ package tulingxueyuan;
 
 import java.util.ArrayList;
 import java.util.Collections;
+/*
+* 桶排序：将一组数分成若干份，然后利用数求出每份之间的范围差，然后将在符合范围的数进行插入，因为存在不断地插入数，所以可以使用LinkList和ArrayList链表来进行对桶数的存储和桶内数的存储。
+过程：首先找出数中的最大值和最小值，然后求出桶数和间隔数，之后创建链表将桶数存入，将数中的每个数存入到对应的桶内，对每个桶内进行排序，从0号桶开始，将每个桶内排好顺序的数放入到一个新的数组中，最后打印数组即可
 
+桶数计算：(max-min)/数组长度+1
+间隔数：(max-min+1)/数组长度
+————————————————
+版权声明：本文为CSDN博主「小波（学习版）」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/weixin_54174102/article/details/123392174
+* https://blog.csdn.net/weixin_54174102/article/details/123392174?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522165193403616781483713250%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=165193403616781483713250&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduend~default-1-123392174-null-null.142^v9^pc_search_result_control_group,157^v4^control&utm_term=%E6%A1%B6%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95java&spm=1018.2226.3001.4187*/
 public class BuckerSort2 {
 
     public static void buckerSort(int[]a){
