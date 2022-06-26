eb4639088ddd85cc906e66dee4f1188c0ba6684e
liuxing <2487334399@qq.com>
2022-06-26 23:31:40 +0800

    提交netty拷贝的案例

--- NettyPro/src/com/atgs/nio/BasicBuffer.java
+++ NettyPro/src/com/atgs/nio/BasicBuffer.java
@@ -3,10 +3,12 @@ package com.atgs.nio;
 import java.nio.IntBuffer;
 
 public class BasicBuffer {
+
     public static void main(String[] args) {
         //举例说明Buffer的使用(简单说明)
         //创建一个Buffer,大小为5，即可以存放5个int
         IntBuffer intBuffer=IntBuffer.allocate(5);
+
         //向buffer存放数据
         /* intBuffer.put(10);
         intBuffer.put(11);
@@ -19,8 +21,12 @@ public class BasicBuffer {
         //如何从buffer读取数据
         //将buffer转换，读写切换
         intBuffer.flip();
+
         while(intBuffer.hasRemaining()){
             System.out.print(intBuffer.get()+" ");
         }
+
+
     }
+
 }
